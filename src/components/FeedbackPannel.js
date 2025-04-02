7d667bee8e86bbdc1f1f3acdd3561e891c094d33
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2025-04-02 18:03:39 +0200

    Create FeedbackPannel.js

---
+++ src/components/FeedbackPannel.js
@@ -0,0 +1,11 @@
+import SectionTitle from "./SectionTitle";
+
+function FeedbackPannel() {
+  return (
+    <section>
+      <SectionTitle>Wieści się roznoszą</SectionTitle>
+    </section>
+  );
+}
+
+export default FeedbackPannel;
