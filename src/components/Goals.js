cf347c594d6fcb685ab71b521c0f87994996e41f
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2025-04-02 18:03:34 +0200

    Create Goals.js

---
+++ src/components/Goals.js
@@ -0,0 +1,11 @@
+import SectionTitle from "./SectionTitle"
+
+function Goals() {
+    return (
+       <section>
+        <SectionTitle>Cele na horyzoncie</SectionTitle>
+       </section>
+    )
+}
+
+export default Goals
