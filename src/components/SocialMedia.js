4646931c8144a51439ed8e6f40913ec1d9ac62ac
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2025-04-23 17:38:39 +0200

    Update SocialMedia.js

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,71 +1,83 @@
+"use client";
+
+import { useState } from "react";
 import SectionTitle from "./SectionTitle";
 import "../styles/socialMediaSection.css";
 import Image from "next/image";
 function SocialMedia() {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleToggle() {
+    setIsOpen((prev) => !prev);
+    console.log(isOpen);
+  }
   return (
     <section className="social-media-section">
-      <SectionTitle>Social Media ostatnie posty</SectionTitle>
-      <div className="posts-wrapper">
-        <div className="platform-block">
-          <div className="post">
-            <Image
-              src="/post-1.svg"
-              alt="Post Instagram 1"
-              width={200}
-              height={200}
-            />
-            <p>Lorem ipsum dolor sit amet consectetur…</p>
-            <a href="https://www.instagram.com/post1" target="_blank">
-              zobacz
-            </a>
+      <SectionTitle onClick={handleToggle}>
+        Social Media ostatnie posty
+      </SectionTitle>
+      {isOpen && (
+        <div className="posts-wrapper">
+          <div className="platform-block">
+            <div className="post">
+              <Image
+                src="/post-1.svg"
+                alt="Post Instagram 1"
+                width={200}
+                height={200}
+              />
+              <p>Lorem ipsum dolor sit amet consectetur…</p>
+              <a href="https://www.instagram.com/post1" target="_blank">
+                zobacz
+              </a>
+            </div>
+            <div className="post">
+              <Image
+                src="/post-2.svg"
+                alt="Post Instagram 2"
+                width={200}
+                height={200}
+              />
+              <p>Lorem ipsum dolor sit amet consectetur…</p>
+              <a href="https://www.instagram.com/post2" target="_blank">
+                zobacz
+              </a>
+            </div>
           </div>
-          <div className="post">
-            <Image
-              src="/post-2.svg"
-              alt="Post Instagram 2"
-              width={200}
-              height={200}
-            />
-            <p>Lorem ipsum dolor sit amet consectetur…</p>
-            <a href="https://www.instagram.com/post2" target="_blank">
-              zobacz
-            </a>
-          </div>
-        </div>
 
-        <div className="icons-separator">
-          <Image src="Instagram.svg" alt="Instagram" width={48} height={48} />
-          <span></span>
-          <Image src="/LinkedIn.svg" alt="LinkedIn" width={48} height={48} />
-        </div>
+          <div className="icons-separator">
+            <Image src="Instagram.svg" alt="Instagram" width={48} height={48} />
+            <span></span>
+            <Image src="/LinkedIn.svg" alt="LinkedIn" width={48} height={48} />
+          </div>
 
-        <div className="platform-block">
-          <div className="post">
-            <Image
-              src="/post-3.svg"
-              alt="Post LinkedIn 1"
-              width={200}
-              height={200}
-            />
-            <p>Lorem ipsum dolor sit amet consectetur…</p>
-            <a href="https://www.linkedin.com/post1" target="_blank">
-              zobacz
-            </a>
-          </div>
-          <div className="post">
-            <Image
-              src="/post-4.svg"
-              alt="Post LinkedIn 2"
-              width={200}
-              height={200}
-            />
-            <p>Lorem ipsum dolor sit amet consectetur…</p>
-            <a href="https://www.linkedin.com/post2" target="_blank">
-              zobacz
-            </a>
+          <div className="platform-block">
+            <div className="post">
+              <Image
+                src="/post-3.svg"
+                alt="Post LinkedIn 1"
+                width={200}
+                height={200}
+              />
+              <p>Lorem ipsum dolor sit amet consectetur…</p>
+              <a href="https://www.linkedin.com/post1" target="_blank">
+                zobacz
+              </a>
+            </div>
+            <div className="post">
+              <Image
+                src="/post-4.svg"
+                alt="Post LinkedIn 2"
+                width={200}
+                height={200}
+              />
+              <p>Lorem ipsum dolor sit amet consectetur…</p>
+              <a href="https://www.linkedin.com/post2" target="_blank">
+                zobacz
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 }
