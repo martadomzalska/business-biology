324331e369d712f41227f34bac02b48fdbd89bed
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2025-04-23 17:38:45 +0200

    Update Goals.js

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,11 +1,64 @@
-import SectionTitle from "./SectionTitle"
+"use client";
 
-function Goals() {
-    return (
-       <section>
-        <SectionTitle>Cele na horyzoncie</SectionTitle>
-       </section>
-    )
-}
+import { useState } from "react";
+import SectionTitle from "./SectionTitle";
+import "../styles/goalsSection.css";
+import Image from "next/image";
 
-export default Goals
+const Goals = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleToggle() {
+    setIsOpen((prev) => !prev);
+    console.log(isOpen);
+  }
+  return (
+    <section>
+      <SectionTitle onClick={handleToggle}>Cele na horyzoncie</SectionTitle>
+      {isOpen && (
+        <div className="goals-section">
+          <div className="goal">
+            <Image
+              src="./goals.svg"
+              alt="goal"
+              width={218}
+              height={231}
+              className="img"
+            />
+            <p className="paragraphh">
+              Lorem ipsum dolor sit amet consectetur...
+            </p>
+            <button className="button">Zobacz</button>
+          </div>
+          <div className="goal">
+            <Image
+              src="./goals.svg"
+              alt="goal"
+              width={218}
+              height={231}
+              className="img"
+            />
+            <p className="paragraphh">
+              Lorem ipsum dolor sit amet consectetur...
+            </p>
+            <button className="button">Zobacz</button>
+          </div>
+          <div className="goal">
+            <Image
+              src="./goals.svg"
+              alt="goal"
+              width={218}
+              height={231}
+              className="img"
+            />
+            <p className="paragraphh">
+              Lorem ipsum dolor sit amet consectetur...
+            </p>
+            <button className="button">zobacz</button>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Goals;
