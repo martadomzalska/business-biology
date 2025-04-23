54ca6ab8a95a095a5b63d54e7c33acfa33092da7
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2025-04-23 17:38:51 +0200

    Update BusinessBiology.js

--- src/components/BusinessBiology.js
+++ src/components/BusinessBiology.js
@@ -1,32 +1,42 @@
+"use client";
+
+import { useState } from "react";
 import "../styles/businessBiology.css";
 import SectionTitle from "./SectionTitle";
 
 const BusinessBiology = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleToggle() {
+    setIsOpen((prev) => !prev);
+    console.log(isOpen);
+  }
   return (
     <section className="business-biology">
-      <SectionTitle>Business Biology</SectionTitle>
-      <div className="cards-wrapper">
-        <div className="card right">
-          <h3>Biology</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Sollicitudin odio egestas
-            leo pharetra purus sed mauris. Sollicitudin odio egestas leo
-            pharetra purus sed mauris. Sollicitudin odio egestas leo pharetra
-            purus sed mauris.
-          </p>
-          <button>button</button>
+      <SectionTitle onClick={handleToggle}>Business Biology</SectionTitle>
+      {isOpen && (
+        <div className="cards-wrapper">
+          <div className="card right">
+            <h3>Biology</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Sollicitudin odio egestas
+              leo pharetra purus sed mauris. Sollicitudin odio egestas leo
+              pharetra purus sed mauris. Sollicitudin odio egestas leo pharetra
+              purus sed mauris.
+            </p>
+            <button>button</button>
+          </div>
+          <div className="card left">
+            <h3>Business</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Sollicitudin odio egestas
+              leo pharetra purus sed mauris. Sollicitudin odio egestas leo
+              pharetra purus sed mauris. Sollicitudin odio egestas leo pharetra
+              purus sed mauris.
+            </p>
+            <button>button</button>
+          </div>
         </div>
-        <div className="card left">
-          <h3>Business</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Sollicitudin odio egestas
-            leo pharetra purus sed mauris. Sollicitudin odio egestas leo
-            pharetra purus sed mauris. Sollicitudin odio egestas leo pharetra
-            purus sed mauris.
-          </p>
-          <button>button</button>
-        </div>
-      </div>
+      )}
     </section>
   );
 };
