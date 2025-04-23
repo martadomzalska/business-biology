33c684e9a67a2d6dfc4aeb8baeb4d28e43f27693
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2025-04-23 17:38:33 +0200

    Update Timeline.js

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,11 +1,78 @@
-import SectionTitle from "./SectionTitle"
+"use client";
+
+import { useState } from "react";
+import SectionTitle from "./SectionTitle";
+import Image from "next/image";
+import "../styles/timelineSection.css";
 
 function Timeline() {
-    return (
-      <section>
-        <SectionTitle>Timeline</SectionTitle>
-      </section>
-    )
+  const [isOpen, setIsOpen] = useState(true);
+  function handleToggle() {
+    setIsOpen((prev) => !prev);
+    console.log(isOpen);
+  }
+  return (
+    <section>
+      <SectionTitle onClick={handleToggle}>Timeline</SectionTitle>
+      {isOpen && (
+        <div className="timeline-section">
+          <Image
+            src="./spiral.svg"
+            alt="spiral"
+            width={64}
+            height={58}
+            className="spiral"
+          />
+          <Image src="./Arrow.svg" alt="arrow" width={264} height={1763.5} />
+          <Image
+            src="./timeline-1.svg"
+            alt="timeline1"
+            width={400}
+            height={400}
+            className="timelineImg first"
+          />
+          <Image
+            src="./timeline-2.svg"
+            alt="timeline2"
+            width={400}
+            height={400}
+            className="timelineImg second"
+          />
+          <Image
+            src="./timeline-3.svg"
+            alt="timeline3"
+            width={400}
+            height={400}
+            className="timelineImg third"
+          />
+          <p className="paragraph one">
+            Lorem ipsum dolor sit amet consectetur. Suscipit amet ut congue
+            consectetur duis neque turpis nec aenean..
+          </p>
+          <p className="paragraph two">
+            Lorem ipsum dolor sit amet consectetur. Suscipit amet ut congue
+            consectetur duis neque turpis nec aenean..
+          </p>
+          <p className="paragraph three">
+            Lorem ipsum dolor sit amet consectetur. Suscipit amet ut congue
+            consectetur duis neque turpis nec aenean..
+          </p>
+          <p className="paragraph four">
+            Lorem ipsum dolor sit amet consectetur. Suscipit amet ut congue
+            consectetur duis neque turpis nec aenean..
+          </p>
+          <p className="paragraph five">
+            Lorem ipsum dolor sit amet consectetur. Suscipit amet ut congue
+            consectetur duis neque turpis nec aenean..
+          </p>
+          <p className="paragraph six">
+            Lorem ipsum dolor sit amet consectetur. Suscipit amet ut congue
+            consectetur duis neque turpis nec aenean..
+          </p>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default Timeline
+export default Timeline;
