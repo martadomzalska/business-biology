6922095f3c79252ae073e1ee95f541aee97a0723
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2025-04-23 17:38:43 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import SectionTitle from "./SectionTitle";
 import Image from "next/image";
 import { useKeenSlider } from "keen-slider/react";
@@ -27,6 +28,12 @@ const projects = [
 ];
 
 const ProjectsSection = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleToggle() {
+    setIsOpen((prev) => !prev);
+    console.log(isOpen);
+  }
+
   const [sliderRef, instanceRef] = useKeenSlider({
     slides: { perView: 3, spacing: 135 },
     loop: true,
@@ -34,54 +41,57 @@ const ProjectsSection = () => {
 
   return (
     <section className="projects-section">
-      <SectionTitle>Projekty</SectionTitle>
-
-      <div className="carousel-wrapper">
-        <button
-          className="nav-button left"
-          onClick={() => instanceRef.current?.prev()}
-        >
-          <Image
-            src="/left-slider.svg"
-            alt="left-slider"
-            width={24}
-            height={553}
-          />
-        </button>
-
-        <div ref={sliderRef} className="keen-slider">
-          {projects.map((project, index) => (
-            <div className="keen-slider__slide project-slide" key={index}>
+      <SectionTitle onClick={handleToggle}>Projekty</SectionTitle>
+      {isOpen && (
+        <>
+          <div className="carousel-wrapper">
+            <button
+              className="nav-button left"
+              onClick={() => instanceRef.current?.prev()}
+            >
               <Image
-                src={project.image}
-                alt={project.title}
-                width={256}
-                height={256}
-                className="project-image"
+                src="/left-slider.svg"
+                alt="left-slider"
+                width={24}
+                height={553}
               />
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <a href="#" className="project-link">
-                zobacz
-              </a>
+            </button>
+
+            <div ref={sliderRef} className="keen-slider">
+              {projects.map((project, index) => (
+                <div className="keen-slider__slide project-slide" key={index}>
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    width={256}
+                    height={256}
+                    className="project-image"
+                  />
+                  <h3>{project.title}</h3>
+                  <p>{project.description}</p>
+                  <a href="#" className="project-link">
+                    zobacz
+                  </a>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
 
-        <button
-          className="nav-button right"
-          onClick={() => instanceRef.current?.next()}
-        >
-          <Image
-            src="/right-slider.svg"
-            alt="right-slider"
-            width={24}
-            height={553}
-          />
-        </button>
-      </div>
+            <button
+              className="nav-button right"
+              onClick={() => instanceRef.current?.next()}
+            >
+              <Image
+                src="/right-slider.svg"
+                alt="right-slider"
+                width={24}
+                height={553}
+              />
+            </button>
+          </div>
 
-      <button className="all-projects-button">wszystkie projekty</button>
+          <button className="all-projects-button">wszystkie projekty</button>
+        </>
+      )}
     </section>
   );
 };
